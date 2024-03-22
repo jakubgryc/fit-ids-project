3663fc3f384d0d3abaa7acbf9892947690b7269f
nuk <marek.g.sterba@gmail.com>
2024-03-22 16:08:38 +0100

    naplneni daty hotove

--- part2/xsterb16_xgrycj03.sql
+++ part2/xsterb16_xgrycj03.sql
@@ -19,21 +19,21 @@ drop table kavarna;
 -- Vytvoreni tabulek
 
 create table osoba(
-    id_osoby int primary key,
+    id_osoby int primary key not null,
     jmeno nvarchar2(255) not null,
     prijmeni nvarchar2(255) not null,
     datum_narozeni date not null,
     telefon nvarchar2(14) not null check ( regexp_like(telefon, '^(\+[0-9]{12})|([0-9]{9})$')),
-    email nvarchar2(255) not null check ( regexp_like(email, '^[a-z0-9.-_]+@[a-z0-9.-_]+]\.[a-z]+]$'))
+    email nvarchar2(255) not null check ( regexp_like(email, '^[a-z0-9.\-_]+@[a-z0-9.\-_]+\.[a-z0-9.\-_]+$'))
 );
 
 create table zamestnanec(
-    id_osoby int primary key references osoba(id_osoby) on delete cascade,
+    id_osoby int primary key references osoba(id_osoby),
     pozice nvarchar2(255) not null check ( pozice in ('majitel', 'barista', 'cisnik', 'kuchar'))
 );
 
 create table uzivatel(
-    id_osoby int primary key references osoba(id_osoby) on delete cascade,
+    id_osoby int primary key references osoba(id_osoby),
     oblibeny_druh_kavy nvarchar2(255) not null,
     oblibeny_druh_pripravy nvarchar2(255) not null,
     kav_denne int not null check (kav_denne >= 0)
@@ -48,7 +48,7 @@ create table kavarna(
     cislo_popisne  nvarchar2(255) not null,
     kapacita int not null check (kapacita > 0),
     popis nvarchar2(1023) not null,
-    cas_otevreni nvarchar2(5) not null,
+    cas_otevreni nvarchar2(5) not null check(regexp_like(cas_otevreni, '^[0-9]{2}:[0-9]{2}$')),
     cas_zavreni nvarchar2(5) not null
 );
 
@@ -110,3 +110,41 @@ create table zamestnanec_pracuje_v_kavarne(
     id_zamestnance int not null references zamestnanec(id_osoby),
     primary key (id_kavarny, id_zamestnance)
 );
+
+-- naplneni ukazkovymi daty
+
+insert into kavarna(id_kavarny, nazev, mesto, psc, ulice, cislo_popisne, kapacita, popis, cas_otevreni, cas_zavreni)
+values (1, 'Dobre rano', 'Brno', '61200', 'Purkynova', '1234', '30', 'Nove otevrena kavarna v retro stylu', '08:00', '21:00');
+
+insert into kava(nazev, oblast_puvodu, popis_chuti, zpusob_pripravy)
+values ('Raj na zemi', 'Novy Zeland', 'Intenzivni aromaticka', 'espresso');
+
+insert into kavarna_nabizi_kavu(id_kavarny, nazev_kavy)
+values (1, 'Raj na zemi');
+
+insert into osoba(id_osoby, jmeno, prijmeni, datum_narozeni, telefon, email)
+values (1, 'Josef', 'Novak', to_date('12.12.2000', 'dd.mm.yyyy'), '+420731054882', 'josef.novak@gmail.com');
+
+insert into uzivatel(id_osoby, oblibeny_druh_kavy, oblibeny_druh_pripravy, kav_denne)
+values (1, 'cappucino', 'french press', 2);
+
+insert into uzivatel_oblibena_kavarna(id_kavarny, id_uzivatele)
+values (1, 1);
+
+insert into osoba(id_osoby, jmeno, prijmeni, datum_narozeni, telefon, email)
+values (2, 'Libor', 'Hrncir', to_date('11.11.1999', 'dd.mm.yyyy'), '+420739425872', 'libor.hrncir@email.cz');
+
+insert into zamestnanec(id_osoby, pozice)
+values (2, 'barista');
+
+insert into zamestnanec_pracuje_v_kavarne(id_kavarny, id_zamestnance)
+values (1, 2);
+
+insert into recenze(id_recenze, datum_vytvoreni, pocet_hvezdicek, text_recenze, palce_nahoru, palce_dolu, id_uzivatele, id_kavarny)
+values (1, to_date('01.03.2024', 'dd.mm.yyyy'), 5, 'Super kavarna na klidnem miste, mila obsluha. Urcite se tu jeste zastavim.', 0, 0, 1, 1);
+
+insert into reakce(id_reakce, text_reakce, palce_nahoru, palce_dolu, id_zamestnance, id_recenze)
+values (1, 'To nas tesi, dekujeme za zpetnou vazbu!', 1, 0, 2, 1);
+
+insert into uzivatel_ohodnotil_reakci(id_uzivatele, id_reakce)
+values (1, 1);
