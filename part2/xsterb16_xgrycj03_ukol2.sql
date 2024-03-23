e029618f8de7697c38b38427df7a1839e13a5f4d
nuk <marek.g.sterba@gmail.com>
2024-03-23 15:43:00 +0100

    generovane primarni klice

--- part2/xsterb16_xgrycj03_ukol2.sql
+++ part2/xsterb16_xgrycj03_ukol2.sql
@@ -19,7 +19,7 @@ drop table kavarna;
 -- Vytvoreni tabulek
 
 create table osoba(
-    id_osoby int primary key not null,
+    id_osoby int generated as identity primary key not null,
     jmeno nvarchar2(255) not null,
     prijmeni nvarchar2(255) not null,
     datum_narozeni date not null,
@@ -40,7 +40,7 @@ create table uzivatel(
 );
 
 create table kavarna(
-    id_kavarny int primary key not null,
+    id_kavarny int generated as identity primary key not null,
     nazev nvarchar2(255) not null,
     mesto nvarchar2(255) not null,
     psc nvarchar2(255) not null,
@@ -53,7 +53,7 @@ create table kavarna(
 );
 
 create table recenze(
-    id_recenze int primary key not null,
+    id_recenze int generated as identity primary key not null,
     datum_vytvoreni date not null,
     pocet_hvezdicek int check ((pocet_hvezdicek <= 5) and (pocet_hvezdicek >= 1)),
     text_recenze nvarchar2(1023) not null,
@@ -64,7 +64,7 @@ create table recenze(
 );
 
 create table reakce(
-    id_reakce int primary key,
+    id_reakce int generated as identity primary key,
     text_reakce nvarchar2(1023) not null,
     palce_nahoru int not null check (palce_nahoru >= 0),
     palce_dolu int not null check (palce_dolu >= 0),
@@ -113,8 +113,8 @@ create table zamestnanec_pracuje_v_kavarne(
 
 -- naplneni ukazkovymi daty
 
-insert into kavarna(id_kavarny, nazev, mesto, psc, ulice, cislo_popisne, kapacita, popis, cas_otevreni, cas_zavreni)
-values (1, 'Dobre rano', 'Brno', '61200', 'Purkynova', '1234', '30', 'Nove otevrena kavarna v retro stylu', '08:00', '21:00');
+insert into kavarna(nazev, mesto, psc, ulice, cislo_popisne, kapacita, popis, cas_otevreni, cas_zavreni)
+values ('Dobre rano', 'Brno', '61200', 'Purkynova', '1234', '30', 'Nove otevrena kavarna v retro stylu', '08:00', '21:00');
 
 insert into kava(nazev, oblast_puvodu, popis_chuti, zpusob_pripravy)
 values ('Raj na zemi', 'Novy Zeland', 'Intenzivni aromaticka', 'espresso');
@@ -122,8 +122,8 @@ values ('Raj na zemi', 'Novy Zeland', 'Intenzivni aromaticka', 'espresso');
 insert into kavarna_nabizi_kavu(id_kavarny, nazev_kavy)
 values (1, 'Raj na zemi');
 
-insert into osoba(id_osoby, jmeno, prijmeni, datum_narozeni, telefon, email)
-values (1, 'Josef', 'Novak', to_date('12.12.2000', 'dd.mm.yyyy'), '+420731054882', 'josef.novak@gmail.com');
+insert into osoba(jmeno, prijmeni, datum_narozeni, telefon, email)
+values ('Josef', 'Novak', to_date('12.12.2000', 'dd.mm.yyyy'), '+420731054882', 'josef.novak@gmail.com');
 
 insert into uzivatel(id_osoby, oblibeny_druh_kavy, oblibeny_druh_pripravy, kav_denne)
 values (1, 'cappucino', 'french press', 2);
@@ -131,8 +131,8 @@ values (1, 'cappucino', 'french press', 2);
 insert into uzivatel_oblibena_kavarna(id_kavarny, id_uzivatele)
 values (1, 1);
 
-insert into osoba(id_osoby, jmeno, prijmeni, datum_narozeni, telefon, email)
-values (2, 'Libor', 'Hrncir', to_date('11.11.1999', 'dd.mm.yyyy'), '+420739425872', 'libor.hrncir@email.cz');
+insert into osoba(jmeno, prijmeni, datum_narozeni, telefon, email)
+values ('Libor', 'Hrncir', to_date('11.11.1999', 'dd.mm.yyyy'), '+420739425872', 'libor.hrncir@email.cz');
 
 insert into zamestnanec(id_osoby, pozice)
 values (2, 'barista');
@@ -140,11 +140,11 @@ values (2, 'barista');
 insert into zamestnanec_pracuje_v_kavarne(id_kavarny, id_zamestnance)
 values (1, 2);
 
-insert into recenze(id_recenze, datum_vytvoreni, pocet_hvezdicek, text_recenze, palce_nahoru, palce_dolu, id_uzivatele, id_kavarny)
-values (1, to_date('01.03.2024', 'dd.mm.yyyy'), 5, 'Super kavarna na klidnem miste, mila obsluha. Urcite se tu jeste zastavim.', 0, 0, 1, 1);
+insert into recenze(datum_vytvoreni, pocet_hvezdicek, text_recenze, palce_nahoru, palce_dolu, id_uzivatele, id_kavarny)
+values (to_date('01.03.2024', 'dd.mm.yyyy'), 5, 'Super kavarna na klidnem miste, mila obsluha. Urcite se tu jeste zastavim.', 0, 0, 1, 1);
 
-insert into reakce(id_reakce, text_reakce, palce_nahoru, palce_dolu, id_zamestnance, id_recenze)
-values (1, 'To nas tesi, dekujeme za zpetnou vazbu!', 1, 0, 2, 1);
+insert into reakce(text_reakce, palce_nahoru, palce_dolu, id_zamestnance, id_recenze)
+values ('To nas tesi, dekujeme za zpetnou vazbu!', 1, 0, 2, 1);
 
 insert into uzivatel_ohodnotil_reakci(id_uzivatele, id_reakce)
 values (1, 1);
